165db78bfbc85ef1aa66bbd38a51f7b26c6ad86a
ruth206 <robinsonruth307@gmail.com>
2023-12-29 12:07:18 +0000

    h

--- COMP1001-master/23_24_coursework/Report/question3/VS/code_to_start/main.cpp
+++ COMP1001-master/23_24_coursework/Report/question3/VS/code_to_start/main.cpp
@@ -26,7 +26,7 @@ void openfile(const char* filename, FILE** finput);
 int getint(FILE* fp);
 
 //CRITICAL POINT: images' paths - You need to change these paths
-#define IN "C:\\Users\\vboxuser\\source\\repos\\image_processing\\image_processing\\input_images\\a1.pgm"
+#define IN "C:\Users\robin\COMP1001\COMP1001-master\23_24_coursework\Report\question3\VS\code_to_start\input_images\a0.pgm" /*"C:\\Users\\vboxuser\\source\\repos\\image_processing\\image_processing\\input_images\\a1.pgm"*/
 #define OUT "C:\\Users\\vboxuser\\source\\repos\\image_processing\\image_processing\\output_images\\blurred.pgm"
 #define OUT2 "C:\\Users\\vboxuser\\source\\repos\\image_processing\\image_processing\\output_images\\edge_detection.pgm"
 
